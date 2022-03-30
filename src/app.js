11d3e9adc7bc7286caa418a9eec85ff8094e2e56
Laura <laucarrillo21@gmail.com>
2022-03-30 12:03:28 -0600

    Added search engine

--- src/app.js
+++ src/app.js
@@ -43,10 +43,21 @@ function displayTemperature (response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search (city) {
+    let apiKey = "7c0a7bf7120da0d635ff53ea1528f155";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 
-let apiKey = "7c0a7bf7120da0d635ff53ea1528f155";
-let city = "Sydney"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+}
+
+function handleSubmit (event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York");
 
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("search-form");
+form.addEventListener ("submit", handleSubmit);
