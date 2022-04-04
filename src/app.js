8f042177412f428eae86697d017a7c8dc59e49a9
Laura <laucarrillo21@gmail.com>
2022-04-04 07:48:35 -0500

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -59,5 +59,5 @@ function handleSubmit (event) {
 search("New York");
 
 
-let form = document.querySelector("search-form");
+let form = document.querySelector("#search-form");
 form.addEventListener ("submit", handleSubmit);
